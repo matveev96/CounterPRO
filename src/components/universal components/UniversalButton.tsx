e3c0cd35efe265ed72270d72a44019f69049ccca
matveev96 <sasha.matveev1996@icloud.com>
2024-12-16 11:45:27 +0300

    add pseudo-class active to button

--- src/components/universal components/UniversalButton.tsx
+++ src/components/universal components/UniversalButton.tsx
@@ -15,13 +15,14 @@ export const UniversalButton = ({onClick, title, isDisabled}: UniversalButtonPro
 };
 
 const ButtonStyled = styled.button<{disabled: boolean}>`
-    margin: 20px 0;
-    width: 40%;
-    height: 30%;
+    padding: 5px 20px;
     border: 3px solid cornflowerblue;
     border-radius: 10px;
     background-color: ${props => props.disabled ? "transparent" : "cornflowerblue"};
     color: azure;
     cursor: ${props => props.disabled ? "not-allowed": "pointer"};
     font-size: 30px;
+    &:active{
+        padding: 3px 15px;
+    }
 `
