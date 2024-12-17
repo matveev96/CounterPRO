b0238a26c5d52d67cb5840a75840d9754f5f5537
matveev96 <sasha.matveev1996@icloud.com>
2024-12-17 16:11:16 +0300

    refactoring of project after codereview: add all sates to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,63 @@
-import React, {useEffect, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import './App.css';
 import styled from "styled-components";
 import {Counter} from "./components/Counter";
 import {Settings} from "./components/Settings";
 
-export type ValueType = {
-    startValue: number,
-    maxValue: number
-}
-
 function App() {
 
-    const [value, setValue] = useState<ValueType>({"startValue": 0, "maxValue": 5})
-    const [message, setMessage] = useState<string>("")
+    const [startValue, setStartValue] = useState<number>(0)
+    const [maxValue, setMaxValue] = useState<number>(5)
 
-    const updateValue = (newValue: ValueType) => {
-        setValue({...value, startValue: newValue.startValue, maxValue: newValue.maxValue})
+    const [startSettings, setStartSettings] = useState<number>(startValue)
+    const [maxSettings, setMaxSettings] = useState<number>(maxValue)
+
+    const [message, setMessage] = useState<string>("")
+    const [count, setCount] = useState<number>(startValue)
+    const [isDisabled, setIsDisabled] = useState(false);
+
+    const counterAdd = () => {
+        if (count < maxValue) {
+            setCount(count + 1)
+        }
+    }
+
+    const counterReset = () => {
+        setCount(startValue)
+    }
+
+    const updateDisabled = () => {
+        setIsDisabled(true)
+        localStorage.setItem("disabledButton", JSON.stringify(true));
+    }
+
+    const updateStartValue = (newValue: number) => {
+        setStartValue(newValue)
+        localStorage.setItem("startValue", JSON.stringify(newValue));
+    }
+    const updateMaxValue = (newValue: number) => {
+        setMaxValue(newValue)
+        localStorage.setItem("maxValue", JSON.stringify(newValue));
+    }
+
+    const updateOnClick = () => {
+        updateStartValue(startSettings)
+        setCount(startSettings)
+        updateMaxValue(maxSettings)
+        updateMassage("")
+        updateDisabled()
+    }
+
+    const updateOnChangeMaxValue = (e: ChangeEvent<HTMLInputElement>) => {
+        const inputValue = +(e.currentTarget.value)
+        setMaxSettings(inputValue)
+        setIsDisabled(false)
+    }
+
+    const updateOnChangeStartValue = (e: ChangeEvent<HTMLInputElement>) => {
+        const inputValue = +(e.currentTarget.value)
+        setStartSettings(inputValue)
+        setIsDisabled(false)
     }
 
     const updateMassage = (newString: string) => {
@@ -23,28 +65,65 @@ function App() {
     }
 
     useEffect(() => {
-        let valueToString = localStorage.getItem("counterValue");
-        if (valueToString) {
-            const valueToObject = JSON.parse(valueToString)
-            setValue(valueToObject)
+        const startValueToString = localStorage.getItem("startValue");
+        if (startValueToString) {
+            const startValueToObject = JSON.parse(startValueToString)
+            setStartValue(startValueToObject)
+            setStartSettings(startValueToObject)
+            setCount(startValueToObject)
+        }
+
+        const maxValueToString = localStorage.getItem("maxValue");
+        if (maxValueToString) {
+            const maxValueToObject = JSON.parse(maxValueToString)
+            setMaxValue(maxValueToObject)
+            setMaxSettings(maxValueToObject)
+        }
+        const disabledButtonToString = localStorage.getItem("disabledButton");
+        if (disabledButtonToString) {
+            const disabledButtonToObject = JSON.parse(disabledButtonToString)
+            setIsDisabled(disabledButtonToObject)
         }
     }, []);
 
+    const numberInputErrorOne = startSettings >= maxSettings
+    const numberInputErrorTwo = startSettings < 0
+
+    const messageToggle = () => {
+        numberInputErrorOne || numberInputErrorTwo ?
+            updateMassage("Incorrect value!") :
+            updateMassage("Enter values and press 'set'")
+    }
+
     useEffect(() => {
-        localStorage.setItem("counterValue", JSON.stringify(value));
-    }, [value]);
+        if (startSettings !== startValue ) {
+            messageToggle()
+        }
+    }, [startSettings]);
+
+    useEffect(() => {
+        if (maxSettings !== maxValue) {
+            messageToggle()
+        }
+    }, [maxSettings]);
 
     return (
         <AppStyled>
-            <Settings updateValue={updateValue}
-                      updateMassage={updateMassage}
-                      value={value}
+            <Settings updateOnClick={updateOnClick}
+                      updateOnChangeMaxValue={updateOnChangeMaxValue}
+                      updateOnChangeStartValue={updateOnChangeStartValue}
+                      numberInputErrorOne={numberInputErrorOne}
+                      numberInputErrorTwo={numberInputErrorTwo}
+                      startSettings={startSettings}
+                      maxSettings={maxSettings}
+                      isDisabled={isDisabled}
             />
-            <Counter startValue={value.startValue}
-                     maxValue={value.maxValue}
+            <Counter counterAdd={counterAdd}
+                     counterReset={counterReset}
+                     count={count}
+                     maxValue={maxValue}
                      message={message}
             />
-
         </AppStyled>
     );
 }
@@ -53,8 +132,7 @@ const AppStyled = styled.div`
     text-align: center;
     display: flex;
     flex-wrap: wrap;
-    gap: 20px;
-    height: 100vh;
+    height: 100%;
     justify-content: space-evenly;
     align-items: center;
     background-color: dimgrey;
