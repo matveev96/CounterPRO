5d6e23dce5cd86ffe29866878c40c24d2583c4ec
matveev96 <sasha.matveev1996@icloud.com>
2024-12-17 16:17:38 +0300

    refactoring of project after codereview: edit styles

--- src/components/universal components/Wrapper.tsx
+++ src/components/universal components/Wrapper.tsx
@@ -14,8 +14,9 @@ export const Wrapper = (props: WrapperPropsType) => {
 };
 
 const WrapperStyled = styled.div`
-    width: 30vw;
-    height: 60vh;
+    width: 500px;
+    height: 500px;
+    margin: 50px;
     border: 3px solid cornflowerblue;
     border-radius: 10px;
     display: flex;
