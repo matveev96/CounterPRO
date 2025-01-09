0dda3e1ffbd6da1e12f9d164f3dbc1fb0aeb15d2
matveev96 <sasha.matveev1996@icloud.com>
2025-01-09 21:29:56 +0300

    add App with Redux

--- src/app/AppWithRedux.tsx
+++ src/app/AppWithRedux.tsx
@@ -1,53 +1,93 @@
-import React, {ChangeEvent, useEffect, useState} from 'react';
+import React, {ChangeEvent, useEffect} from 'react';
 import './App.css';
-import {Counter} from "./components/Counter";
-import {Settings} from "./components/Settings";
+import {Counter} from "../components/Counter";
+import {Settings} from "../components/Settings";
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid2';
 import {createTheme, ThemeProvider} from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import {MaterialUISwitch} from "./components/MUISwitchDarkMode";
+import {MaterialUISwitch} from "../components/MUISwitchDarkMode";
+import {RootState} from "./store";
+import {
+    setAddCounterAC,
+    setIsDisabledAC, setMaxSettingsAC,
+    setMaxValueAC, setMessageAC,
+    setResetCounterAC, setStartSettingsAC,
+    setStartValueAC, setThemeModeAC
+} from "../model/app-reducer";
+import {useAppDispatch, useAppSelector} from "./hooks";
 
-type ThemeMode = 'dark' | 'light'
+export type ThemeMode = 'dark' | 'light'
 
+export type CounterType = {
+    startValue: number
+    maxValue: number
+    startSettings: number
+    maxSettings: number
+    count: number
+    message: string
+    isDisabled: boolean
+    themeMode: ThemeMode
+}
+
+const selectCounter = (state: RootState): CounterType => state.counter
 
 function App() {
 
-    const [startValue, setStartValue] = useState(0)
-    const [maxValue, setMaxValue] = useState(5)
+    const {
+        startValue,
+        maxValue,
+        startSettings,
+        maxSettings,
+        count,
+        message,
+        isDisabled,
+        themeMode,
+    } = useAppSelector(selectCounter);
 
-    const [startSettings, setStartSettings] = useState(startValue)
-    const [maxSettings, setMaxSettings] = useState(maxValue)
+    const dispatch = useAppDispatch()
 
-    const [message, setMessage] = useState("")
-    const [count, setCount] = useState(startValue)
-    const [isDisabled, setIsDisabled] = useState(false);
-
-    const [themeMode, setThemeMode] = useState<ThemeMode>('light')
+    // const [startValue, setStartValue] = useState(0)
+    // const [maxValue, setMaxValue] = useState(5)
+    //
+    // const [startSettings, setStartSettings] = useState(startValue)
+    // const [maxSettings, setMaxSettings] = useState(maxValue)
+    //
+    // const [message, setMessage] = useState("")
+    // const [count, setCount] = useState(startValue)
+    // const [isDisabled, setIsDisabled] = useState(false);
+    //
+    // const [themeMode, setThemeMode] = useState<ThemeMode>('light')
 
     const counterAdd = () => {
         if (count < maxValue) {
-            setCount(prevState => prevState + 1)
+            // setCount(prevState => prevState + 1)
+            dispatch(setAddCounterAC())
         }
     }
 
     const counterReset = () => {
-        setCount(startValue)
+        // setCount(startValue)
+        dispatch(setResetCounterAC())
     }
 
     const updateDisabled = () => {
-        setIsDisabled(true)
+        // setIsDisabled(true)
+        dispatch(setIsDisabledAC(true))
         localStorage.setItem("disabledButton", JSON.stringify(true));
     }
 
     const updateStartValue = (newValue: number) => {
-        setStartValue(newValue)
+        // setStartValue(newValue)
+        dispatch(setStartValueAC(newValue))
         localStorage.setItem("startValue", JSON.stringify(newValue));
+
     }
 
     const updateMaxValue = (newValue: number) => {
-        setMaxValue(newValue)
+        // setMaxValue(newValue)
+        dispatch(setMaxValueAC(newValue))
         localStorage.setItem("maxValue", JSON.stringify(newValue));
     }
 
@@ -56,47 +96,52 @@ function App() {
         updateMaxValue(maxSettings)
         updateMassage("")
         updateDisabled()
-        setCount(startSettings)
+        // setCount(startSettings)
     }
 
     const updateOnChangeMaxValue = (e: ChangeEvent<HTMLInputElement>) => {
-        const inputValue = +(e.currentTarget.value)
-        setMaxSettings(inputValue)
-        setIsDisabled(false)
+        const inputValue = +e.currentTarget.value
+        // setMaxSettings(inputValue)
+        // setIsDisabled(false)
+        dispatch(setMaxSettingsAC(inputValue))
+        dispatch(setIsDisabledAC(false))
     }
 
     const updateOnChangeStartValue = (e: ChangeEvent<HTMLInputElement>) => {
         const inputValue = +e.currentTarget.value ?? '0'
-        setStartSettings(inputValue)
-        setIsDisabled(false)
+        // setStartSettings(inputValue)
+        // setIsDisabled(false)
+        dispatch(setStartSettingsAC(inputValue))
+        dispatch(setIsDisabledAC(false))
     }
 
     const updateMassage = (newString: string) => {
-        setMessage(newString)
+        // setMessage(newString)
+        dispatch(setMessageAC(newString))
     }
 
-    useEffect(() => {
-        const startValueToString = localStorage.getItem("startValue");
-        if (startValueToString) {
-            const startValueToObject = JSON.parse(startValueToString)
-            setStartValue(startValueToObject)
-            setStartSettings(startValueToObject)
-            setCount(startValueToObject)
-        }
-
-        const maxValueToString = localStorage.getItem("maxValue");
-        if (maxValueToString) {
-            const maxValueToObject = JSON.parse(maxValueToString)
-            setMaxValue(maxValueToObject)
-            setMaxSettings(maxValueToObject)
-        }
-
-        const disabledButtonToString = localStorage.getItem("disabledButton");
-        if (disabledButtonToString) {
-            const disabledButtonToObject = JSON.parse(disabledButtonToString)
-            setIsDisabled(disabledButtonToObject)
-        }
-    }, []);
+    // useEffect(() => {
+    //     const startValueToString = localStorage.getItem("startValue");
+    //     if (startValueToString) {
+    //         const startValueToObject = JSON.parse(startValueToString)
+    //         setStartValue(startValueToObject)
+    //         setStartSettings(startValueToObject)
+    //         setCount(startValueToObject)
+    //     }
+    //
+    //     const maxValueToString = localStorage.getItem("maxValue");
+    //     if (maxValueToString) {
+    //         const maxValueToObject = JSON.parse(maxValueToString)
+    //         setMaxValue(maxValueToObject)
+    //         setMaxSettings(maxValueToObject)
+    //     }
+    //
+    //     const disabledButtonToString = localStorage.getItem("disabledButton");
+    //     if (disabledButtonToString) {
+    //         const disabledButtonToObject = JSON.parse(disabledButtonToString)
+    //         setIsDisabled(disabledButtonToObject)
+    //     }
+    // }, []);
 
     const numberInputErrorOne = startSettings >= maxSettings
     const numberInputErrorTwo = startSettings < 0
@@ -122,20 +167,21 @@ function App() {
 
 
     const changeModeHandler = () => {
-        setThemeMode(themeMode === 'light' ? 'dark' : 'light')
+        // setThemeMode(themeMode === 'light' ? 'dark' : 'light')
+        dispatch(setThemeModeAC())
     }
 
     const theme = createTheme({
         palette: {
             mode: themeMode === 'light' ? 'light' : 'dark',
-                primary: {
-                    main: themeMode === 'light' ? '#66bb6a' : '#388e3c',
-                    contrastText: themeMode === 'light' ? '#000' : '#f5f5f5'
-                },
-                secondary: {
-                    main: themeMode === 'light' ? '#7986cb' : '#455a64',
-                    contrastText: themeMode === 'light' ? '#26a69a' : '#00695c'
-                },
+            primary: {
+                main: themeMode === 'light' ? '#66bb6a' : '#388e3c',
+                contrastText: themeMode === 'light' ? '#000' : '#f5f5f5'
+            },
+            secondary: {
+                main: themeMode === 'light' ? '#7986cb' : '#455a64',
+                contrastText: themeMode === 'light' ? '#26a69a' : '#00695c'
+            },
             background: {
                 paper: themeMode === 'light' ? '#e0e0e0' : '#424242',
                 default: themeMode === 'light' ? '#e0e0e0' : '#424242',
