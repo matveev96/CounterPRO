ac803565b127e45506de1989e2da2c5c59c68598
matveev96 <sasha.matveev1996@icloud.com>
2025-01-27 17:18:16 +0300

    refactor App.tsx

---
+++ src/app/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {ThemeProvider} from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline';
+import {useAppSelector} from "./hooks";
+import {getTheme} from "../common/theme/theme";
+import {Main} from "./Main";
+import {selectCounter} from "../features/counter/model/app-selectors";
+
+
+function App() {
+
+    const {themeMode} = useAppSelector(selectCounter);
+
+    return (
+        <ThemeProvider theme={getTheme(themeMode)}>
+            <CssBaseline/>
+            <Main/>
+        </ThemeProvider>
+    );
+}
+
+
+export default App;
