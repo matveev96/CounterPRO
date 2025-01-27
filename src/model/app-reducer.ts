045381d0579ab0294bd0d2558967e1a943a15ec5
matveev96 <sasha.matveev1996@icloud.com>
2025-01-27 15:51:56 +0300

    change Redux on RTK reducer

--- src/model/app-reducer.ts
+++ src/model/app-reducer.ts
@@ -1,4 +1,5 @@
 import {CounterType, ThemeMode} from "../app/AppWithRedux";
+import {createAction, createReducer} from "@reduxjs/toolkit";
 
 const initialState: CounterType = {
     startValue: 0,
@@ -11,149 +12,39 @@ const initialState: CounterType = {
     themeMode: 'light'
 }
 
-export const appReducer = (state: CounterType = initialState, action: ActionType): CounterType => {
-    switch (action.type) {
-        case "SET_START_VALUE": {
-            return {
-                ...state,
-                startValue: action.payload.newValue,
-            }
-        }
-        case "SET_START_SETTINGS": {
-            return {
-                ...state,
-                startSettings: action.payload.newValue,
-            }
-        }
-        case "SET_MAX_VALUE": {
-            return {
-                ...state,
-                maxValue: action.payload.newMaxValue,
-            }
-        }
-        case "SET_MAX_SETTINGS": {
-            return {
-                ...state,
-                maxSettings: action.payload.newMaxValue
-            }
-        }
-        case "SET_COUNT": {
-            return {
-                ...state,
-                count: action.payload.newCount
-            }
-        }
-        case "SET_MESSAGE": {
-            return {
-                ...state,
-                message: action.payload.newString
-            }
-        }
-        case "SET_IS_DISABLED": {
-            return {
-                ...state,
-                isDisabled: action.payload.bool
-            }
-        }
-        case "SET_THEME_MODE": {
-            return {
-                ...state,
-                themeMode: action.payload.darkMode
-            }
-        }
-        default:
-            return state
-    }
-}
+export const setStartValueAC = createAction<{ newValue: number }>('counter/startValue')
+export const setMaxValueAC = createAction<{ newMaxValue: number }>('counter/maxValue')
+export const setStartSettingsAC = createAction<{ newValue: number }>('counter/startSettings')
+export const setMaxSettingsAC = createAction<{ newMaxValue: number }>('counter/maxSettings')
+export const setCountAC = createAction<{ newCount: number }>('counter/count')
+export const setMessageAC = createAction<{ newString: string }>('counter/message')
+export const setIsDisabledAC = createAction<{ bool: boolean }>('counter/isDisabled')
+export const setThemeModeAC = createAction<{ darkMode: ThemeMode }>('counter/themeMode')
 
-export const setStartValueAC = (newValue: number) => {
-    return (
-        {
-            type: 'SET_START_VALUE',
-            payload: {
-                newValue
-            }
-        } as const)
-}
-export const setStartSettingsAC = (newValue: number) => {
-    return (
-        {
-            type: 'SET_START_SETTINGS',
-            payload: {
-                newValue
-            }
-        } as const)
-}
-export const setMaxValueAC = (newMaxValue: number) => {
-    return (
-        {
-            type: 'SET_MAX_VALUE',
-            payload: {
-                newMaxValue
-            }
-        } as const)
-}
-export const setMaxSettingsAC = (newMaxValue: number) => {
-    return (
-        {
-            type: 'SET_MAX_SETTINGS',
-            payload: {
-                newMaxValue
-            }
-        } as const)
-}
-export const setCountAC = (newCount: number) => {
-    return (
-        {
-            type: 'SET_COUNT',
-            payload: {
-                newCount
-            }
-        } as const)
-}
-export const setMessageAC = (newString: string) => {
-    return (
-        {
-            type: 'SET_MESSAGE',
-            payload: {
-                newString
-            }
-        } as const)
-}
-export const setIsDisabledAC = (bool: boolean) => {
-    return (
-        {
-            type: 'SET_IS_DISABLED',
-            payload: {
-                bool
-            }
-        } as const)
-}
-export const setThemeModeAC = (darkMode: ThemeMode) => {
-    return (
-        {
-            type: 'SET_THEME_MODE',
-            payload: {
-                darkMode
-            }
-        } as const)
-}
-
-type setStartValueAT = ReturnType<typeof setStartValueAC>
-type setStartSettingsAT = ReturnType<typeof setStartSettingsAC>
-type setMaxValueAT = ReturnType<typeof setMaxValueAC>
-type setMaxSettingsAT = ReturnType<typeof setMaxSettingsAC>
-type setMessageAT = ReturnType<typeof setMessageAC>
-type setIsDisabledAT = ReturnType<typeof setIsDisabledAC>
-type setThemeModeAT = ReturnType<typeof setThemeModeAC>
-type setCountAT = ReturnType<typeof setCountAC>
-
-type ActionType =
-    setStartValueAT
-    | setStartSettingsAT
-    | setMaxValueAT
-    | setMaxSettingsAT
-    | setMessageAT
-    | setIsDisabledAT
-    | setThemeModeAT
-    | setCountAT
+export const appReducer = createReducer(initialState, builder => {
+    builder
+        .addCase(setStartValueAC, (state, action) => {
+            state.startValue = action.payload.newValue
+        })
+        .addCase(setMaxValueAC, (state, action) => {
+            state.maxValue = action.payload.newMaxValue
+        })
+        .addCase(setStartSettingsAC, (state, action) => {
+            state.startSettings = action.payload.newValue
+        })
+        .addCase(setMaxSettingsAC, (state, action) => {
+            state.maxSettings = action.payload.newMaxValue
+        })
+        .addCase(setCountAC, (state, action) => {
+            state.count = action.payload.newCount
+        })
+        .addCase(setMessageAC, (state, action) => {
+            state.message = action.payload.newString
+        })
+        .addCase(setIsDisabledAC, (state, action) => {
+            state.isDisabled = action.payload.bool
+        })
+        .addCase(setThemeModeAC, (state, action) => {
+            state.themeMode = action.payload.darkMode
+        })
+})
